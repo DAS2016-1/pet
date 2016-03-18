5b384c786a1b55eed9d3adf95b0d0f1c4beb8ca3
Lucas Severo <lucassalves65@gmail.com>
2016-03-18 17:39:48 -0300

    initial recipe for pet finished
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- cookbooks/pet/recipes/default.rb
+++ cookbooks/pet/recipes/default.rb
@@ -35,7 +35,7 @@ cookbook_file '/etc/postgresql/9.4/main/pg_hba.conf' do
 end
 
 service 'postgresql' do
-  action :start
+  action [:start, :enable]
 end
 
 execute "createuser --createdb pet" do
@@ -63,7 +63,22 @@ cookbook_file '/etc/hosts' do
   action :create
 end
 
+service 'postgresql' do
+  action :restart
+end
+
 execute "/vagrant/pet-update -c" do
+  user "pet"
+  action :run
+end
+
+execute "database tables" do
+  command "psql pet --command \"INSERT INTO team (name, maintainer, url) VALUES ('pkg-perl', 'Debian Perl Group <pkg-perl-maintaine    rs@lists.alioth.debian.org>', 'http://pkg-perl.alioth.debian.org/'); INSERT INTO repository (name, type, root, web_root, team_id) VALUES     ('git','git','https://pet.alioth.debian.org/pet2-data/pkg-perl/git-pkg-perl-packages.json','http://anonscm.debian.org/gitweb/?p=pkg-per    l/packages', 1); INSERT INTO package (name, repository_id) VALUES ('clive', 1); INSERT INTO archive (name, url, web_root) VALUES ('debia    n', 'http://cdn.debian.net/debian', 'http://packages.qa.debian.org/'); INSERT INTO suite (archive_id, name) VALUES (1, 'unstable');\""
+  user "pet"
+  action :run
+end
+
+execute "/srv/pet/pet-serve" do
   user "pet"
   action :run
 end
