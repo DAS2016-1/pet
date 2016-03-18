5ebdc2bb474f7d7eb377a6540d81a1325f205a6c
Lucas Severo <lucassalves65@gmail.com>
2016-03-18 00:59:59 -0300

    more commands added to recipe
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- cookbooks/pet/recipes/default.rb
+++ cookbooks/pet/recipes/default.rb
@@ -14,3 +14,57 @@ package 'python-subversion'
 package 'python-pip' 
 package 'wget'
 
+execute 'pip install pyramid_chameleon'
+
+user 'pet' do
+  action :create
+end
+
+file '/etc/postgresql/9.4/main/postgresql.conf' do
+  owner 'postgres'
+  group 'postgres'
+  mode '644'
+  action :create
+end
+
+file '/etc/postgresql/9.4/main/pg_hba.conf' do
+  owner 'postgres'
+  group 'postgres'
+  mode '640'
+  action :create
+end
+
+service 'postgresql' do
+  action :start
+end
+
+execute "createuser --createdb pet" do
+  user "postgres"
+  action :run
+  ignore_failure true
+end
+
+execute "createdb pet" do
+  user "pet"
+  action :run
+  ignore_failure true
+end
+
+execute "psql pet < /usr/share/postgresql/9.4/contrib/debversion.sql" do
+  user "postgres"
+  action :run
+  ignore_failure true
+end
+
+file '/etc/hosts' do
+  owner 'root'
+  group 'root'
+  mode '644'
+  action :create
+end
+
+execute "/vagrant/pet-update -c" do
+  user "pet"
+  action :run
+end
+
