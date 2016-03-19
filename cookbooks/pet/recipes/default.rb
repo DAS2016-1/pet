a55384af28cba23d727ce1a5fce06095ddad281a
Daniel Henrique <danielhmarinho@gmail.com>
2016-03-19 17:41:44 -0300

    Solved insert problem in recipe and some refactoring

--- cookbooks/pet/recipes/default.rb
+++ cookbooks/pet/recipes/default.rb
@@ -38,7 +38,8 @@ service 'postgresql' do
   action [:restart, :enable]
 end
 
-execute "createuser pet" do
+execute "create pet user" do
+  command "createuser pet"
   user "postgres"
   action :run
   ignore_failure true
@@ -50,7 +51,8 @@ execute "createdb -O pet pet" do
   ignore_failure true
 end
 
-execute "psql pet < /usr/share/postgresql/9.4/contrib/debversion.sql" do
+execute "implements debversion types" do
+  command "psql pet < /usr/share/postgresql/9.4/contrib/debversion.sql"
   user "postgres"
   action :run
   ignore_failure true
@@ -67,18 +69,38 @@ service 'postgresql' do
   action :restart
 end
 
-execute "/vagrant/pet-update -c" do
+execute "update schema" do
+  command "/vagrant/pet-update -c"
   user "pet"
   action :run
 end
 
-execute "database tables" do
-  command "psql pet --command \"INSERT INTO team (name, maintainer, url) VALUES ('pkg-perl', 'Debian Perl Group <pkg-perl-maintaine    rs@lists.alioth.debian.org>', 'http://pkg-perl.alioth.debian.org/'); INSERT INTO repository (name, type, root, web_root, team_id) VALUES     ('git','git','https://pet.alioth.debian.org/pet2-data/pkg-perl/git-pkg-perl-packages.json','http://anonscm.debian.org/gitweb/?p=pkg-per    l/packages', 1); INSERT INTO package (name, repository_id) VALUES ('clive', 1); INSERT INTO archive (name, url, web_root) VALUES ('debia    n', 'http://cdn.debian.net/debian', 'http://packages.qa.debian.org/'); INSERT INTO suite (archive_id, name) VALUES (1, 'unstable');\""
+execute "database insert team table" do
+  command "psql pet --command \"INSERT INTO team (name, maintainer, url) VALUES ('pkg-perl', 'Debian Perl Group <pkg-perl-maintainers@lists.alioth.debian.org>', 'http://pkg-perl.alioth.debian.org/');\""
   user "pet"
   action :run
 end
 
-execute "/srv/pet/pet-serve" do
+execute "database insert repository table" do
+  command "psql pet --command \"INSERT INTO repository (name, type, root, web_root, team_id) VALUES ('git','git','https://pet.alioth.debian.org/pet2-data/pkg-perl/git-pkg-perl-packages.json','http://anonscm.debian.org/gitweb/?p=pkg-perl/packages', 1);\""
+  user "pet"
+  action :run
+end
+
+execute "database insert package table" do
+  command "psql pet --command \"INSERT INTO package (name, repository_id) VALUES ('clive', 1);\""
+  user "pet"
+  action :run
+end
+
+execute "database insert archive table" do
+  command "psql pet --command \"INSERT INTO archive (name, url, web_root) VALUES ('debian', 'http://cdn.debian.net/debian', 'http://packages.qa.debian.org/');\""
+  user "pet"
+  action :run
+end
+
+execute "database insert suite table" do
+  command "psql pet --command \"INSERT INTO suite (archive_id, name) VALUES (1, 'unstable');\""
   user "pet"
   action :run
 end
