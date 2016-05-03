1b529ea4b556b9b1ef003cff6da4cdb8d3045703
Lucas Severo <lucassalves65@gmail.com>
2016-05-03 19:51:20 -0300

    tests for ssl cert flag
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

---
+++ test/test_models.py
@@ -0,0 +1,37 @@
+import unittest
+import pet.models
+import pet
+from sqlalchemy.exc import OperationalError
+import os.path
+from mock import MagicMock
+
+class TestPetModels(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_check_ssl_certificate_with_file(self):
+        os.path.isfile = MagicMock(return_value=True)
+        pet.engine = MagicMock(return_value=True)
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNotNone(engine)
+    
+    def test_check_ssl_certificate_with_file_ex(self):
+        os.path.isfile = MagicMock(return_value=True)
+        pet.engine = MagicMock(side_effect=OperationalError("", {}, None))
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNone(engine)
+
+    def test_check_ssl_certificate_no_file(self):
+        os.path.isfile = MagicMock(return_value=False)
+        pet.engine = MagicMock(return_value=True)
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNotNone(engine)
+
+    def test_check_ssl_certificate_no_file_ex(self):
+        os.path.isfile = MagicMock(return_value=False)
+        pet.engine = MagicMock(side_effect=OperationalError("", {}, None))
+        engine = pet.models.check_ssl_certificate()
+        self.assertIsNone(engine)
+
+
